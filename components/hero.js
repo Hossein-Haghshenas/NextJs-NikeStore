f9376aae54bd29d9324f0a954df3aaef78806a21
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-11-25 20:04:07 +0330

    Create hero component

---
+++ components/hero.js
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+const Hero = () => {
+
+    return (
+        <>
+            <section>
+                <section>
+
+                </section>
+                <section>
+                    <section>
+                        <h1>Play With Electric Nike</h1>
+                        <h1>Adapt2.0Sneakers</h1>
+                        <button>Explore Product</button>
+                        <section></section>
+                        <section></section>
+                    </section>
+                    <section>
+                        <Image className='' alt="hero" />
+                    </section>
+                </section>
+            </section>
+        </>
+    )
+}
+
+export default Hero

4909cb35ff3dabbcf791c9268bb916a5dc4aa8c8
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-11-27 11:27:37 +0330

    Fix mp4 error in nextjs with webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mp4/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/[hash][ext]',
+      },
+    })
+
+    return config
+},
 }
 
 module.exports = nextConfig

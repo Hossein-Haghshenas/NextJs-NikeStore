1f3bf7da6f1e045536c056101a7d396580814bda
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-19 11:06:26 +0330

    Add content for tailwind config to read classNames from metaData

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./data/index.js"],
   theme: {
     extend: {},
     screens: {
-      'xl': { 'max': '1200px' },
-      'lg': { 'max': '991px' },
-      'md': { 'max': '767px' },
-      'sm': { 'max': '550px' },
-      'xsm': { 'max': '375px' },
-    }
+      xl: { max: "1200px" },
+      lg: { max: "991px" },
+      md: { max: "767px" },
+      sm: { max: "550px" },
+      xsm: { max: "375px" },
+    },
   },
   plugins: [],
-}
+};

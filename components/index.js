93c5affa7ab38eba027d466360c0e7c3a48b87ec
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-19 11:09:10 +0330

    Create reusable component for sales sections

--- components/index.js
+++ components/index.js
@@ -1,1 +1,2 @@
-export { default as Hero } from './hero'
+export { default as Hero } from "./hero";
+export { default as Sales } from "./sales";
---
+++ components/sales.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./utils/card";
+import Title from "./utils/title";
+import clsx from "clsx";
+
+const Sales = ({ data, isPopularSection }) => {
+  const { title, items } = data;
+  return (
+    <>
+      <section className="nike-container">
+        <Title title={title} />
+        <section
+          className={clsx(
+            "grid items-center justify-items-center  gap-7 lg:gap-5 mt-7",
+            isPopularSection ? "grid-cols-3 xl:grid-cols-2 sm:grid-cols-1" : "grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1"
+          )}
+        >
+          {items?.map((item, index) => {
+            return <Card key={item.id} {...item} isPopularSection={isPopularSection} />;
+          })}
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default Sales;

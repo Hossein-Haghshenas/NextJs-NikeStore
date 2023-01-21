415fb1eb647fc2ed346949996821297b6747e83e
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-21 19:05:34 +0330

    Create reducer for add product to cart

--- components/app/cartSlice.js
+++ components/app/cartSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cartState: false,
+  cartItems: [],
 };
 
 const cartSlice = createSlice({
@@ -14,11 +15,21 @@ const cartSlice = createSlice({
     setCloseCart: (state, action) => {
       state.cartState = action.payload.cartState;
     },
+    setAddItemToCart: (state, action) => {
+      const itemIndex = state.cartItems.findIndex((item) => item.id === action.payload.id);
+
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].productCount += 1;
+      } else {
+        const temp = { ...action.payload, productCount: 1 };
+        state.cartItems.push(temp);
+      }
+    },
   },
 });
 
 export const cartState = (state) => state.cart.cartState;
 
-export const { setOpenCart, setCloseCart } = cartSlice.actions;
+export const { setOpenCart, setCloseCart, setAddItemToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;

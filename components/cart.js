15fb13efe59684e6a7cfd55093ac0e84fe747dd0
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-21 18:41:36 +0330

    Set style for show/unShow cart and Implement onClose functionality for cart

--- components/cart.js
+++ components/cart.js
@@ -1,12 +1,28 @@
 import clsx from "clsx";
+import { useDispatch, useSelector } from "react-redux";
+import { cartState, setCloseCart } from "./app/cartSlice";
 import { CartCount, CartEmpty, CartItem } from "./cartOptions";
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const cartVisibility = useSelector(cartState);
+  const onCartToggle = () => {
+    dispatch(
+      setCloseCart({
+        cartState: false,
+      })
+    );
+  };
   return (
     <>
-      <section className={clsx("fixed top-0 left-0 right-0 bottom-0 blur-effect-theme w-full h-screen opacity-100 z-[350]")}>
+      <section
+        className={clsx(
+          "fixed top-0 left-0 right-0 bottom-0 blur-effect-theme w-full h-screen opacity-100 z-[350]",
+          cartVisibility ? "opacity-100 visible translate-x-0" : "opacity-0 invisible translate-x-8"
+        )}
+      >
         <section className={clsx("blur-effect-theme h-screen max-w-xl w-full absolute right-0")}>
-          <CartCount />
+          <CartCount onCartToggle={onCartToggle} />
           <CartEmpty />
           <CartItem />
         </section>
--- components/cartOptions/cartCount.js
+++ components/cartOptions/cartCount.js
@@ -1,7 +1,7 @@
 import { ChevronDoubleLeftIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
-const CartCount = () => {
+const CartCount = ({ onCartToggle }) => {
   return (
     <>
       <section className="bg-white h-11 flex items-center justify-between px-3 sticky top-0 left-0 right-0 w-full">
@@ -16,7 +16,7 @@ const CartCount = () => {
           </section>
         </section>
         <section className="flex items-center">
-          <button type="button" className="rounded bg-theme-cart active:scale-90 p-0.5">
+          <button onClick={onCartToggle} type="button" className="rounded bg-theme-cart active:scale-90 p-0.5">
             <XMarkIcon className="w-5 h-5 text-white stroke-[2]" />
           </button>
         </section>

386c26925dda581fb57654afeadeb5707e2a7cd6
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-22 17:16:03 +0330

    Create toaster in app file to use that in everywhere

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import { Poppins } from "@next/font/google";
 import { Provider } from "react-redux";
 import Store from "../components/app/store";
+import { Toaster } from "react-hot-toast";
 
 const poppins = Poppins({ subsets: ["latin"], style: ["normal", "italic"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={Store}>
+      <Toaster position="top-center" reverseOrder={false} />
       <section className={poppins.className}>
         <Component {...pageProps} />
       </section>

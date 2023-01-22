003bd58427dac08dbfcb9afbec08cf4f7607f31e
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-22 17:19:02 +0330

    Create cart products add & remove functionality &  also implement that in the local storage

--- components/app/cartSlice.js
+++ components/app/cartSlice.js
@@ -1,8 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+
+// set cart products to the localStorage and use that
+const connectToLocalStorage = (data) => {
+  let haveData;
+  if (typeof window !== "undefined") {
+    // Perform localStorage action
+    data && localStorage.setItem("cart", JSON.stringify(data));
+    haveData = localStorage.getItem("cart");
+  }
+  return haveData ? JSON.parse(localStorage.getItem("cart")) : [];
+};
 
 const initialState = {
   cartState: false,
-  cartItems: [],
+  cartItems: connectToLocalStorage(),
 };
 
 const cartSlice = createSlice({
@@ -20,16 +32,27 @@ const cartSlice = createSlice({
 
       if (itemIndex >= 0) {
         state.cartItems[itemIndex].productCount += 1;
+        toast.success(`Item Count Increased`);
       } else {
         const temp = { ...action.payload, productCount: 1 };
         state.cartItems.push(temp);
+        // create toast message
+        toast.success(`${action.payload.title} added to Cart`);
       }
+      connectToLocalStorage(state.cartItems);
+    },
+    setRemoveItemFromCart: (state, action) => {
+      const removeItem = state.cartItems.filter((item) => item.id !== action.payload.id);
+      state.cartItems = removeItem;
+      connectToLocalStorage(state.cartItems);
+      toast.success(`${action.payload.title} Removed from Cart`);
     },
   },
 });
 
 export const cartState = (state) => state.cart.cartState;
+export const cartProducts = (state) => state.cart.cartItems;
 
-export const { setOpenCart, setCloseCart, setAddItemToCart } = cartSlice.actions;
+export const { setOpenCart, setCloseCart, setAddItemToCart, setRemoveItemFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;

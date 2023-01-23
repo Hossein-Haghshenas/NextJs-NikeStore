a0ab7449f48ba940b04822bf45cfb46814553423
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-23 13:39:12 +0330

    Show total amount in cart & Pass total count to the cart count

--- components/cart.js
+++ components/cart.js
@@ -1,12 +1,19 @@
 import clsx from "clsx";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { cartProducts, cartState, setClearProductsCount, setCloseCart } from "./app/cartSlice";
+import { cartProducts, cartState, cartTotalAmount, cartTotalCount, setClearProductsCount, setCloseCart, setGetTotals } from "./app/cartSlice";
 import { CartCount, CartEmpty, CartItem } from "./cartOptions";
 
 const Cart = () => {
   const dispatch = useDispatch();
   const cartVisibility = useSelector(cartState);
   const products = useSelector(cartProducts);
+  const totalAmount = useSelector(cartTotalAmount);
+  const totalCount = useSelector(cartTotalCount);
+
+  useEffect(() => {
+    dispatch(setGetTotals());
+  }, [products, dispatch]);
 
   const onCartToggle = () => {
     dispatch(
@@ -29,7 +36,7 @@ const Cart = () => {
         )}
       >
         <section className={clsx("blur-effect-theme h-screen max-w-xl w-full absolute right-0")}>
-          <CartCount onClearProductCount={onClearProductCount} onCartToggle={onCartToggle} />
+          <CartCount onClearProductCount={onClearProductCount} onCartToggle={onCartToggle} totalCount={totalCount} />
           {products?.length !== 0 ? (
             <section>
               <section className="flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[81vh] scroll-smooth scroll-hidden py-3">
@@ -41,7 +48,7 @@ const Cart = () => {
               <section className="fixed bottom-0 bg-white w-full px-5 py-2 grid items-center">
                 <section className="flex items-center justify-between">
                   <h2 className="text-base font-semibold uppercase">Total</h2>
-                  <h3 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">000</h3>
+                  <h3 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">{totalAmount}</h3>
                 </section>
                 <section className="grid items-center gap-2">
                   <p className="text-sm font-medium text-center">Taxes and Shipping Will Calculate At Shipping</p>

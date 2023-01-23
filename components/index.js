0b832718c6a507523dae5786c8fd894ceeec7a02
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-23 13:35:46 +0330

    Fix something

--- components/index.js
+++ components/index.js
@@ -3,5 +3,5 @@ export { default as Cart } from "./cart";
 export { default as Hero } from "./hero";
 export { default as Sales } from "./sales";
 export { default as FlexContent } from "./flexContent";
-export { default as Stories } from "./Stories";
+export { default as Stories } from "./stories";
 export { default as Footer } from "./footer";

3c3186ac23d60cf23994d559a59f613ab91f3c0c
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-23 12:23:59 +0330

    Set on cart toggle for btn in cart empty component

--- components/cartOptions/cartEmpty.js
+++ components/cartOptions/cartEmpty.js
@@ -1,12 +1,13 @@
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
-const CartEmpty = () => {
+const CartEmpty = ({ onCartToggle }) => {
   return (
     <>
       <section className="flex items-center justify-center flex-col h-screen px-11 text-center gap-7">
         <Image src="" alt="" className="w-40 lg:w-36 sm:w-28 h-auto object-fill transition-all duration-300 hover:scale-110" />
         <button
+          onClick={onCartToggle}
           type="button"
           className="button-theme bg-gradient-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900 py-2 gap-3 text-sm px-5 font-semibold active:scale-110"
         >
